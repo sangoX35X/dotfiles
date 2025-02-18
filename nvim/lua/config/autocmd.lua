bf3989aa8f2e7985c6e8234f74a90e090825b8f0
sango_35 <sango_35_pro@hotmail.com>
2025-02-19 00:30:11 +0900

    feat(nvim): ✨ typst 用の設定・プラグインの追加
    - 生の Neovim では`*.typ`の filetype が`typst`にならない為, それを自動で
      設定する autocmd を追加。
    - 各言語用のプラグイン用のファイル`languages.lua`を追加。
    - typst 用のプラグイン`typst.vim`を追加。
    - typst のプレビュー用のプラグイン`typst-preview.nvim`を追加。

--- nvim/lua/config/autocmd.lua
+++ nvim/lua/config/autocmd.lua
@@ -13,3 +13,18 @@ vim.api.nvim_create_autocmd(
 		end,
 	}
 )
+
+vim.api.nvim_create_autocmd(
+	{
+		'BufEnter',
+		'BufWinEnter',
+	},
+	{
+		pattern = {
+			'*.typ',
+		},
+		callback = function ()
+			vim.opt_local.filetype = 'typst'
+		end,
+	}
+)
---
+++ nvim/lua/plugin/languages.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		{
+			'kaarmu/typst.vim',
+			ft = 'typst',
+		},
+		{
+			'chomosuke/typst-preview.nvim',
+			cmd = {
+				'TypstPreview',
+				'TypstPreviewFollowCursor',
+				'TypstPreviewFollowCursorToggle',
+				'TypstPreviewNoFollowCursor',
+				'TypstPreviewStop',
+				'TypstPreviewSyncCursor',
+				'TypstPreviewToggle',
+				'TypstPreviewUpdate',
+			}
+		},
+	},
+}
