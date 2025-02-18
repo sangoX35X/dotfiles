29194266d26060bb4f2fa01139054453a7b3e3b8
sango_35 <sango_35_pro@hotmail.com>
2025-02-18 22:15:18 +0900

    refactor(nvim): ♻ ソート
    オプションを、カテゴリ別に名前順にソート。

--- nvim/lua/config/option.lua
+++ nvim/lua/config/option.lua
@@ -6,41 +6,41 @@ vim.opt.encoding = 'utf-8'
 vim.opt.fileencodings = "utf-8,sjis,default,utf-16"
 
 vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = -1
-vim.opt.shiftwidth = 0
 vim.opt.expandtab = false
+vim.opt.shiftwidth = 0
+vim.opt.smartindent = true
+vim.opt.softtabstop = -1
+vim.opt.tabstop = 4
 
 vim.opt.ambiwidth = "single"
 vim.opt.cmdheight = 1
-vim.opt.helplang = "ja,en"
-vim.opt.number = true
-vim.opt.signcolumn = "yes"
-vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
-vim.opt.termguicolors = true
-vim.opt.pumblend = 15
-vim.opt.winblend = 15
+vim.opt.cursorline = true
+vim.opt.helplang = "ja,en"
 vim.opt.list = true
 vim.opt.listchars = {
 	eol = "↵",
 	tab = "  ",
 	space = "･",
 	trail = "␣",
-	extends = "➙"
+	extends = "➙",
 }
+vim.opt.number = true
+vim.opt.pumblend = 15
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.winblend = 15
 vim.opt.wrap = false
 
-vim.opt.wrapscan = true
-vim.opt.showmatch = true
 vim.opt.ignorecase = true
+vim.opt.showmatch = true
 vim.opt.smartcase = true
+vim.opt.wrapscan = true
 
-vim.opt.mousescroll = 'ver:1,hor:1'
 vim.opt.clipboard = "unnamedplus"
+vim.opt.mousescroll = 'ver:1,hor:1'
 vim.opt.virtualedit = "block"
 
-vim.g.use_xhtml = 1
-vim.g.html_use_css = 1
 vim.g.html_no_pre = 1
+vim.g.html_use_css = 1
+vim.g.use_xhtml = 1
