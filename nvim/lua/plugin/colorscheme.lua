9d1b5d2041b43c273b76de10cabb23574d6a182e
sango_35 <sango_35_pro@hotmail.com>
2025-02-18 22:44:36 +0900

    feat(nvim): ✨ カラースキーム`kanagawa.nvim`の追加
    カラースキーム用ファイル`colorscheme.lua`の追加。
    カラースキーム`kanagawa.nvim`を追加し、設定を追加、デフォルトに設定。

---
+++ nvim/lua/plugin/colorscheme.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		config = function ()
+			require 'kanagawa'.setup {
+				compile = true,
+				undercurl = true,
+				commentStyle = {
+					italic = true,
+					bold = false,
+				},
+				functionStyle = {
+					italic = true,
+					bold = false,
+				},
+				keywordStyle = {
+					italic = true,
+					bold = false,
+				},
+				statementStyle = {
+					italic = false,
+					bold = true,
+				},
+				typeStyle = {
+					italic = false,
+					bold = true,
+				},
+			}
+			vim.cmd.colorscheme 'kanagawa'
+		end,
+	},
+}
