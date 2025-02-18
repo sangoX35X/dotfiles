0dd85af7494cd5d4506b6d10e895c264194213f5
sango_35 <sango_35_pro@hotmail.com>
2025-02-18 23:36:50 +0900

    feat(nvim): ✨ プラグイン`skkeleton`の追加
    日本語入力プラグイン`skkeleton`用のファイルを作成し、そのものを追加。

---
+++ nvim/lua/plugin/skkeleton.lua
@@ -0,0 +1,66 @@
+return {
+	'vim-skk/skkeleton',
+	event = {
+		{
+			event = 'User',
+			pattern = 'DenopsReady',
+		},
+	},
+	keys = {
+		{ mode = {'!', 't'}, '<C-j>', '<Plug>(skkeleton-toggle)' },
+	},
+	config = function ()
+		local function callback ()
+			local cache = vim.fn.stdpath('cache') .. '/skkeleton'
+			if not vim.uv.fs_stat(cache) then
+				vim.uv.fs_mkdir(cache, tonumber('777', 8))
+			end
+
+			local selectCanditateKeys = nil
+			if vim.g.colemak then
+				selectCanditateKeys = 'arstnei'
+			end
+
+			local dicts = {
+				'L',
+			}
+			for i = #dicts, 1, -1 do
+				local prefix = 'SKK-JISYO'
+				local share_path = '/usr/share/skk/'
+				local user_path = '~/.skk/'
+
+				local dict = prefix .. '.' .. dicts[i]
+
+				if vim.uv.fs_stat(share_path .. dict) then
+					dict = share_path .. dict
+				elseif vim.uv.fs_stat(user_path .. dict) then
+					dict = user_path .. dict
+				else
+					table.remove(dicts, i)
+				end
+
+				dicts[i] = dict
+			end
+
+			vim.fn['skkeleton#config'] {
+				completionRankFile = cache .. '/rank.json',
+				databasePath = cache .. '/database.json',
+				globalDictionaries = dicts,
+				keepMode = true,
+				registerConvertResult = true,
+				selectCanditateKeys = selectCanditateKeys,
+				sources = {
+					'skk_dictionary',
+					'google_japanese_input',
+				},
+			}
+		end
+		vim.api.nvim_create_autocmd(
+			'User',
+			{
+				pattern = 'skkeleton-initialize-pre',
+				callback = callback,
+			}
+		)
+	end,
+}
