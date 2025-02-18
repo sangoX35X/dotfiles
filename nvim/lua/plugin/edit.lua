18b6e171523048d76d81bc218a8bf1562405f26c
sango_35 <sango_35_pro@hotmail.com>
2025-02-18 23:30:35 +0900

    feat(nvim): ✨ プラグイン`dial.nvim`の追加
    コード編集系プラグイン用のファイル`edit.lua`の追加。
    リテラル等の編集機能の強化の為、`dial.nvim`の追加・設定。
    言語毎の設定に改善の余地。

---
+++ nvim/lua/plugin/edit.lua
@@ -0,0 +1,112 @@
+return {
+	{
+		'monaqa/dial.nvim',
+		keys = {
+			{ '<C-a>' },
+			{ '<C-x>' },
+			{ 'g<C-a>' },
+			{ 'g<C-x>' },
+			{ mode = 'x', '<C-a>' },
+			{ mode = 'x', '<C-x>' },
+			{ mode = 'x', 'g<C-a>' },
+			{ mode = 'x', 'g<C-x>' },
+		},
+		config = function ()
+			local augend = require 'dial.augend'
+			local config = require 'dial.config'
+			local dmap = require 'dial.map'
+			local utils = require 'utils'
+
+			---@param group? string
+			---@return nil
+			local function gen_keymaps (group)
+				utils.set_keymap_from_table {
+					n = {
+						['<C-a>'] = { dmap.inc_normal(group), buffer = true, desc = 'Increment' },
+						['<C-x>'] = { dmap.dec_normal(group), buffer = true, desc = 'Decrement' },
+						['g<C-a>'] = { dmap.inc_gnormal(group), buffer = true, desc = 'Increment' },
+						['g<C-x>'] = { dmap.dec_gnormal(group), buffer = true, desc = 'Decrement' },
+					},
+					x = {
+						['<C-a>'] = { dmap.inc_visual(group), buffer = true, desc = 'Increment' },
+						['<C-x>'] = { dmap.dec_visual(group), buffer = true, desc = 'Decrement' },
+						['g<C-a>'] = { dmap.inc_gvisual(group), buffer = true, desc = 'Increment' },
+						['g<C-x>'] = { dmap.dec_gvisual(group), buffer = true, desc = 'Decrement' },
+					},
+				}
+			end
+
+			local int = augend.integer
+			local const = augend.constant
+			local paren = augend.paren
+			local case = augend.case
+			local semver = augend.semver.alias.semver
+
+			local common = {
+				int.new { natural = false },
+				int.new {
+					natural = false,
+					delimiter = '_',
+				},
+				int.alias.hex,
+				int.alias.octal,
+				int.alias.binary,
+				augend.decimal_fraction.new { signed = true },
+				augend.hexcolor.new { case = 'lower' },
+				const.new { elements = { 'no', 'yes' }, preserve_case = true },
+				const.new { elements = { '-', '+' } },
+				const.new { elements = { '--', '++' }, word = false },
+				const.new { elements = { '-=', '+=' } },
+				const.new { elements = { '<', '>' } },
+				const.new { elements = { '<=', '>=' } },
+			}
+
+			local c_style_eq = const.new { elements = { '!=', '==' } }
+			local common_style_bool = const.alias.bool
+			local python_style_bool = const.new { elements = { 'True', 'False' } }
+
+			config.augends:register_group {
+				default = common,
+				lua = utils.marge_list(
+					common,
+					{
+						paren.alias.lua_str_literal,
+						case.new { types = { 'snake_case', 'camelCase', 'PascalCase' } },
+						const.new { elements = { '~=', '==' } },
+						common_style_bool,
+					}
+					),
+				rust = utils.marge_list(
+					common,
+					{
+						paren.alias.rust_str_literal,
+					}
+					),
+				markdown = {
+					augend.misc.alias.markdown_header,
+				},
+				minifiles = {
+					int.alias.decimal,
+					case.new { types = { 'kebab-case', 'camelCase', 'PascalCase', 'SCREAMING_SNAKE_CASE' } },
+				}
+			}
+
+			vim.api.nvim_create_autocmd(
+				'FileType',
+				{
+					callback = function ()
+						local group_name = nil
+
+						if config.augends:get(vim.bo.filetype) then
+							group_name = vim.bo.filetype
+						end
+
+						gen_keymaps(group_name)
+					end
+				}
+				)
+
+			vim.cmd.set('filetype=' .. vim.bo.filetype)
+		end,
+	},
+}
