c19f0f65d11343f40199f3e6ca067641f6ac363b
sango_35 <sango_35_pro@hotmail.com>
2025-02-22 00:36:33 +0900

    fix(nvim/utils): :ambulance: `utils/list.marge`の修正
    `unpack`を誤用していた為、`ipairs`に変更。

--- nvim/lua/utils/list.lua
+++ nvim/lua/utils/list.lua
@@ -6,8 +6,8 @@ M.marge = function (...)
 	---@type table<number, any>
 	local result = {}
 
-	for list in unpack {...} do
-		for elem in unpack(list) do
+	for _, list in ipairs {...} do
+		for _, elem in ipairs(list) do
 			table.insert(result, elem)
 		end
 	end
