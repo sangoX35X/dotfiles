58ef3e29bb561bf29318df4fad6b680f2c76df59
sango_35 <sango_35_pro@hotmail.com>
2025-02-22 21:06:50 +0900

    fix(nvim/utils): :ambulance: `keymap.set_by_table()`の修正
    - :pencil2: タイプミスによるバグを修正
    
    - :ambulance: `rhs`に`function`を受け付けないようになっていた為、修正

--- nvim/lua/utils/keymap.lua
+++ nvim/lua/utils/keymap.lua
@@ -3,7 +3,7 @@ local M = {}
 ---@alias utils.keymap.mode_string string
 ---@alias utils.keymap.mode string
 ---@alias utils.keymap.lhs string
----@alias utils.keymap.rhs string
+---@alias utils.keymap.rhs string|function
 
 ---@class (exact) utils.keymap.map: vim.keymap.set.Opts
 ---@field [1] utils.keymap.rhs
@@ -27,7 +27,7 @@ M.set_by_table = function (default, keymap_table)
 		---@type utils.keymap.mode[]
 		local modes = {}
 
-		for i = 1, #modes do
+		for i = 1, #mode_string do
 			table.insert(modes, string.sub(mode_string, i, i))
 		end
 
@@ -37,7 +37,8 @@ M.set_by_table = function (default, keymap_table)
 			local rhs
 			---@type vim.keymap.set.Opts
 			local options = setmetatable({}, { __index = default })
-			if type(rhs_or_map) == 'string' then
+			local rhs_or_map_type = type(rhs_or_map)
+			if rhs_or_map_type == 'string' or rhs_or_map_type == 'function' then
 				rhs = rhs_or_map
 			else
 				for key, value in pairs(rhs_or_map) do
