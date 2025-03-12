d99bde05abf71077f3a15cece1f3de0394832ac0
sango_35 <sango_35_pro@hotmail.com>
2025-03-12 15:55:41 +0900

    refactor(nvim/linter): 🎨 linter の設定を分離
    - 🎨 ide.lua から linter に関する設定を削除し、linter.lua に移動

--- nvim/lua/plugin/ide.lua
+++ nvim/lua/plugin/ide.lua
@@ -13,37 +13,4 @@ return {
 			},
 		},
 	},
-	-- Linter
-	{
-		{
-			'mfussenegger/nvim-lint',
-			event = 'VeryLazy',
-			config = function ()
-				local lint = require 'lint'
-				lint.linters_by_ft = {}
-				vim.api.nvim_create_autocmd({
-					'BufEnter',
-					'BufWritePost',
-					'InsertLeave',
-					'TextChanged',
-				}, {
-					callback = function ()
-						local opt = {
-							scope = 'local',
-							buf = 0,
-						}
-						if
-							not vim.api.nvim_get_option_value('readonly', opt)
-							and vim.api.nvim_get_option_value('modifiable', opt)
-						then
-							lint.try_lint(nil, { ignore_errors = true })
-							lint.try_lint({
-								'typos',
-							}, { ignore_errors = true })
-						end
-					end,
-				})
-			end,
-		},
-	},
 }
---
+++ nvim/lua/plugin/linter.lua
@@ -0,0 +1,38 @@
+return {
+	'mfussenegger/nvim-lint',
+	event = 'VeryLazy',
+	opts = {
+		rust = {
+			'clippy',
+		},
+	},
+	config = function (_, opts)
+		local lint = require 'lint'
+
+		lint.linters_by_ft = opts
+
+		local augroup = vim.api.nvim_create_augroup('AutoLint', {})
+
+		vim.api.nvim_create_autocmd({
+			'BufEnter',
+			'BufWritePost',
+			'InsertLeave',
+			'TextChanged',
+		}, {
+			group = augroup,
+			callback = function ()
+				local common_opts_to_get_opt = {
+					scope = 'local',
+					buf = 0,
+				}
+
+				if
+					vim.api.nvim_get_option_value('modifiable', common_opts_to_get_opt)
+					and not vim.api.nvim_get_option_value('readonly', common_opts_to_get_opt)
+				then
+					lint.try_lint(nil, { ignore_errors = true })
+				end
+			end,
+		})
+	end,
+}
