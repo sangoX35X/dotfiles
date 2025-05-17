b72203504f9c79f029205dead2c23eae0ad5807a
sango_35 <sango_35_pro@hotmail.com>
2025-05-17 10:22:24 +0900

    feat(nvim/snacks): ✨ snacks.nvim の為のコミット

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,24 @@
+function _G.debug_inspect (...)
+	Snacks.debug.inspect(...)
+end
+
+function _G.debug_backtrace ()
+	Snacks.debug.backtrace()
+end
+
+vim.print = _G.debug_inspect
+
+do
+	---@type string
+	local random_bytes = vim.uv.random(8)
+	---@type number
+	local random_seed = 0
+	for i = 1, #random_bytes do
+		random_seed = random_seed * 256 + random_bytes:byte(i)
+	end
+	math.randomseed(random_seed)
+end
+
 if vim.loader then
 	vim.loader.enable()
 end
